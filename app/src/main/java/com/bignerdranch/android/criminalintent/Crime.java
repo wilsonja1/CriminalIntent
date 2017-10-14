8833dc1606960baf16992bc937ab85da355a85e2
JackEWilson <jack.wilson.east@gmail.com>
2017-10-14 10:41:29 -0700

    added getters and setters

--- app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -17,4 +17,32 @@ public class Crime {
         mId = UUID.randomUUID();
         mDate = new Date();
     }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
 }
