fcfb842763dd0842b1b01e09d378d6078d6c58d3
JackEWilson <jack.wilson.east@gmail.com>
2017-10-21 12:25:05 -0700

    android manifest added. figure 8.3

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -0,0 +1,11 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by battl on 10/21/2017.
+ */
+
+public class CrimeListFragment extends Fragment {
+    //nothing yet.
+}
