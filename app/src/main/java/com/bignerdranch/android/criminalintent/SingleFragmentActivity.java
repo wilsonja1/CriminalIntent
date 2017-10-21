ddcf1014ad372ed8c1d200adae58f2f7237672be
JackEWilson <jack.wilson.east@gmail.com>
2017-10-21 11:53:08 -0700

    android manifest

--- app/src/main/java/com/bignerdranch/android/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/SingleFragmentActivity.java
@@ -12,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 public abstract class SingleFragmentActivity  extends AppCompatActivity{
 
     protected abstract Fragment createFragment();
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
