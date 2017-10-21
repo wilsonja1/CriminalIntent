488029e6a88dba5104329150eda3001999d03431
JackEWilson <jack.wilson.east@gmail.com>
2017-10-21 13:12:25 -0700

    filled out crimelistfragment.java and fixed compile issue with recycler view

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -1,11 +1,25 @@
 package com.bignerdranch.android.criminalintent;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by battl on 10/21/2017.
  */
 
 public class CrimeListFragment extends Fragment {
-    //nothing yet.
+    private RecyclerView mCrimeRecyclerView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.fragment_crime_list, container, false);
+        mCrimeRecyclerView = (RecyclerView) view.findViewById(R.id.crime_recycler_view);
+        mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        return view;
+    }
 }
