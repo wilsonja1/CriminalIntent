946a75e043860488c05dbede37e6aa26201ebe0f
JackEWilson <jack.wilson.east@gmail.com>
2017-10-24 08:58:32 -0700

    listing 12.7, shows current date initially

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -76,7 +76,8 @@ public class CrimeFragment extends Fragment {
             @Override
             public void onClick(View v){
                 FragmentManager manager = getFragmentManager();
-                DatePickerFragment dialog = new DatePickerFragment();
+                //DatePickerFragment dialog = new DatePickerFragment();
+                DatePickerFragment dialog = DatePickerFragment.newInstance(mCrime.getDate());
                 dialog.show(manager, DIALOG_DATE);
             }
         });
