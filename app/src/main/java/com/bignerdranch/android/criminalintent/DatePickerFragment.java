d2936568fa2df37d0aa9c5ddafc586caec8d7901
JackEWilson <jack.wilson.east@gmail.com>
2017-10-24 08:48:02 -0700

    listing 12.5

--- app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.java
@@ -6,12 +6,28 @@ import android.support.v4.app.DialogFragment;//Changed from android.app.dialogfr
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.DatePicker;
+
+import java.util.Date;
 
 /**
  * Created by battl on 10/23/2017.
  */
 
 public class DatePickerFragment extends DialogFragment {
+    private static final String ARG_DATE = "date";
+    private DatePicker mDatePicker;
+
+    public static DatePickerFragment newInstance(Date date){
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_DATE, date);
+        
+        DatePickerFragment fragment = new DatePickerFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState){
         View v = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_date, null);
