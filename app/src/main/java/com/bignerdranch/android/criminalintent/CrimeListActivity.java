5cc33ff26bb712a565e80f154ddae3f912d722c6
JackEWilson <jack.wilson.east@gmail.com>
2017-11-12 14:15:11 -0800

    added alias resource files. listing 17.5

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListActivity.java
@@ -2,13 +2,14 @@ package com.bignerdranch.android.criminalintent;
 
 import android.support.v4.app.Fragment;
 
-/**
- * Created by battl on 10/21/2017.
- */
-
 public class CrimeListActivity extends SingleFragmentActivity{
     @Override
     protected Fragment createFragment() {
         return new CrimeListFragment();
+    }
+
+    @Override
+    protected int getLayoutResId(){
+        return R.layout.activity_masterdetail;
     }
 }
